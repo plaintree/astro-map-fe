24ffbf37cec5ac3c8bbc4bbdfe4e7ad4bab260fd
plaintree <cheukhei04@hotmail.com>
2023-01-24 13:15:22 +0000

    on progress with select location section

--- screens/LocationFinder.jsx
+++ screens/LocationFinder.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import { View } from "react-native";
 import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
-const LocationFinder = () => {
+import MapView, { MarkerAnimated, Circle } from "react-native-maps";
+
+const LocationFinder = ({ navigation }) => {
+  const [pin, setPin] = useState({ latitude: 51.50722, longitude: -0.1275 });
   return (
-    <SafeAreaView>
-      <Text>LocationFinder</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <MapView
+        initialRegion={{
+          latitude: 51.50722,
+          longitude: -0.1275,
+          latitudeDelta: 0.05,
+          longitudeDelta: 200,
+        }}
+        style={{ width: "100%", height: "50%" }}
+      >
+        <MarkerAnimated
+          coordinate={pin}
+          draggable={true}
+          onDragEnd={(e) =>
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            })
+          }
+          title="London"
+          description="Upcoming Events"
+          onCalloutPress={() =>
+            navigation.navigate("Home", { params: "london" })
+          }
+        />
+      </MapView>
+      <View style={{ paddingTop: 10 }}>
+        <Text
+          variant="headlineLarge"
+          style={{ textAlign: "center", fontWeight: "700" }}
+        >
+          Select Location
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
