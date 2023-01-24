385960da894b7ce6c95aea36c391d6e4e098628c
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-24 15:13:54 +0000

    adds country lists

---
+++ data/worldLocations.js
@@ -0,0 +1,127 @@
+module.exports = [
+    {
+      id: 1,
+      continent: "north-america",
+      countries: "United States",
+    },
+    {
+      id: 2,
+      continent: "north-america",
+      countries: "Canada",
+    },
+    {
+      id: 3,
+      continent: "north-america",
+      countries: "Mexico",
+    },
+    {
+      id: 4,
+      continent: "north-america",
+      countries: "Guatemala",
+    },
+    {
+      id: 5,
+      continent: "north-america",
+      countries: "Cuba",
+    },
+    {
+      id: 6,
+      continent: "south-america",
+      countries: "Argentina",
+    },
+    {
+      id: 7,
+      continent: "south-america",
+      countries: "Bolivia",
+    },
+    {
+      id: 8,
+      continent: "south-america",
+      countries: "Brazil",
+    },
+    {
+      id: 9,
+      continent: "south-america",
+      countries: "Chile",
+    },
+    {
+      id: 10,
+      continent: "south-america",
+      countries: "Colombia",
+    },
+    {
+      id: 11,
+      continent: "europe",
+      countries: "United Kingdom",
+    },
+    {
+      id: 12,
+      continent: "europe",
+      countries: "Germany",
+    },
+    {
+      id: 13,
+      continent: "europe",
+      countries: "France",
+    },
+    {
+      id: 14,
+      continent: "europe",
+      countries: "Spain",
+    },
+    {
+      id: 15,
+      continent: "europe",
+      countries: "Italy",
+    },
+    {
+      id: 16,
+      continent: "africa",
+      countries: "Nigeria",
+    },
+    {
+      id: 17,
+      continent: "africa",
+      countries: "Ethiopia",
+    },
+    {
+      id: 18,
+      continent: "africa",
+      countries: "Egypt",
+    },
+    {
+      id: 19,
+      continent: "africa",
+      countries: "Congo",
+    },
+    {
+      id: 20,
+      continent: "africa",
+      countries: "Tanzania",
+    },
+    {
+      id: 21,
+      continent: "asia",
+      countries: "China",
+    },
+    {
+      id: 22,
+      continent: "asia",
+      countries: "India",
+    },
+    {
+      id: 23,
+      continent: "asia",
+      countries: "Indonesia",
+    },
+    {
+      id: 24,
+      continent: "asia",
+      countries: "Pakistan",
+    },
+    {
+      id: 25,
+      continent: "asia",
+      countries: "Bangladesh",
+    },
+]; 
--- screens/LocationFinder.jsx
+++ screens/LocationFinder.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import { View } from "react-native";
-import { Text, SegmentedButtons } from "react-native-paper";
+import { useState, useEffect } from "react";
+import { View, ScrollView } from "react-native";
+import { Text, SegmentedButtons, List, useTheme, FAB } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { MarkerAnimated, Circle } from "react-native-maps";
+import worldLocations from "../data/worldLocations";
 
 const LocationFinder = ({ navigation }) => {
   const [pin, setPin] = useState({ latitude: 51.50722, longitude: -0.1275 });
-  const [continent, setContinent] = useState("");
+  const [continent, setContinent] = useState("asia");
+  const [locations, setLocations] = useState([])
   const [value, setValue] = useState("");
+  const theme = useTheme();
 
-  console.log(continent)
+  console.log(locations)
+
+  useEffect(() => {
+    const filteredLocations = worldLocations.filter((location) => location.continent === continent);
+    setLocations(filteredLocations);
+  }, [continent]);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -46,6 +54,8 @@ const LocationFinder = ({ navigation }) => {
           Select Location
         </Text>
       </View>
+      <ScrollView>
+
       <SegmentedButtons
         value={value}
         onValueChange={setValue}
@@ -84,6 +94,21 @@ const LocationFinder = ({ navigation }) => {
             onPress: () => setContinent("south-america"),
           },
         ]}/>
+        <List.Section style={{alignItems: "center"}}>
+          {locations.map((location) => {
+            return (
+              <List.Item
+              key={location.id}
+              title={location.countries}
+              titleEllipsizeMode="head"
+              titleStyle={{textAlign: "center"}}
+              onPress={() => console.log("On press")}
+              style={{marginBottom: 5, paddingVertical: 0, borderWidth: 1, borderStyle: "solid", borderColor: theme.colors.outline, backgroundColor: theme.colors.surfaceVariant, borderRadius: 10, width: "50%"}}
+              />
+            )
+          })}
+        </List.Section>
+      </ScrollView>
     </SafeAreaView>
   );
 };
