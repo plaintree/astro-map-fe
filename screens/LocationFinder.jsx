2c7b6d4c461b3e3518ab93a857bf2eb5da068730
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-24 13:47:34 +0000

    adds continent buttons

--- screens/LocationFinder.jsx
+++ screens/LocationFinder.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, SegmentedButtons } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { MarkerAnimated, Circle } from "react-native-maps";
 
 const LocationFinder = ({ navigation }) => {
   const [pin, setPin] = useState({ latitude: 51.50722, longitude: -0.1275 });
+  const [continent, setContinent] = useState("");
+  const [value, setValue] = useState("");
+
+  console.log(continent)
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <MapView
@@ -41,6 +46,44 @@ const LocationFinder = ({ navigation }) => {
           Select Location
         </Text>
       </View>
+      <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        density="small"
+        style={{margin: 10}}
+        buttons={[
+          {
+            value: "europe",
+            label: "Europe",
+            onPress: () => setContinent("europe"),
+          },
+          {
+            value: "africa",
+            label: "Africa",
+            onPress: () => setContinent("africa"),
+          },
+          {
+            value: "asia",
+            label: "Asia",
+            onPress: () => setContinent("asia"),
+          },
+        ]}/>
+        <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        density="small"
+        buttons={[
+          {
+            value: "north-america",
+            label: "North America",
+            onPress: () => setContinent("north-america"),
+          },
+          {
+            value: "south-america",
+            label: "South America",
+            onPress: () => setContinent("south-america"),
+          },
+        ]}/>
     </SafeAreaView>
   );
 };
