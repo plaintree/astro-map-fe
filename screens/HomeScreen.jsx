84c45819dc7d69291e7075662de4960b00a220e8
plaintree <cheukhei04@hotmail.com>
2023-01-30 15:16:24 +0000

    finish adding moon phase from api

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,64 +1,45 @@
+import axios from "axios";
 import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Text } from "react-native-paper";
-import { useRef, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Carousel, { Pagination } from "react-native-snap-carousel";
+import Carousel from "react-native-snap-carousel";
 import CarouselItem, {
   SLIDER_WIDTH,
   ITEM_WIDTH,
 } from "../components/CarouselItem";
 import data from "../data";
-
-const HomeScreenStack = () => {
-  const [index, setIndex] = useState(0);
-  const isCarousel = useRef(null);
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        paddingVertical: 40,
-        alignItems: "center",
-      }}
-    >
-      <StatusBar style="auto" />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingBottom: 20,
-        }}
-      >
-        <View style={{ paddingRight: 20 }}>
-          <Text variant="headlineLarge">Upcoming</Text>
-          <Text variant="headlineLarge">Event</Text>
-        </View>
-        <View style={{ paddingLeft: 20 }}>
-          <Text variant="titleLarge">Moon phase</Text>
-          <Text variant="titleLarge">Partial</Text>
-        </View>
-      </View>
-
-      <Carousel
-        layout="default"
-        layoutCardOffset={9}
-        ref={isCarousel}
-        data={data}
-        renderItem={CarouselItem}
-        sliderWidth={SLIDER_WIDTH}
-        itemWidth={ITEM_WIDTH}
-        inactiveSlideShift={0}
-        useScrollView={true}
-        onSnapToItem={(index) => setIndex(index)}
-      />
-    </SafeAreaView>
-  );
-};
+import moment from "moment";
 
 const HomeScreen = ({ navigation }) => {
-  const [index, setIndex] = useState(0);
+  const [moonPhase, setMoonPhase] = useState("");
+  useEffect(() => {
+    const getMoonPhase = async () => {
+      try {
+        const today = moment().format("YYYY-MM-DD");
+        const res = await axios.get(
+          `https://aa.usno.navy.mil/api/moon/phases/date?date=${today}&nump=1`
+        );
+        setMoonPhase(res.data.phasedata[0].phase);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMoonPhase();
+  }, []);
+
   const isCarousel = useRef(null);
+
+  const getMoonPhaseIcon = (phase) => {
+    let iconStr;
+    if (phase === "Full Moon") return (iconStr = "moon-full");
+    if (phase === "New Moon") return (iconStr = "moon-new");
+    if (phase === "First Quarter") return (iconStr = "moon-first-quarter");
+    if (phase === "Last Quarter") return (iconStr = "moon-last-quarter");
+    return iconStr;
+  };
   return (
     <SafeAreaView
       style={{
@@ -85,8 +66,14 @@ const HomeScreen = ({ navigation }) => {
           </Text>
         </View>
         <View style={{ paddingLeft: 20 }}>
-          <Text variant="titleLarge">Moon phase</Text>
-          <Text variant="titleLarge">Partial</Text>
+          <Text variant="titleLarge">{moment().format("DD MMM YYYY")}</Text>
+          <Text variant="titleLarge">
+            {moonPhase}
+            <MaterialCommunityIcons
+              name={getMoonPhaseIcon(moonPhase)}
+              size={24}
+            />
+          </Text>
         </View>
       </View>
 
@@ -100,7 +87,6 @@ const HomeScreen = ({ navigation }) => {
         itemWidth={ITEM_WIDTH}
         inactiveSlideShift={0}
         useScrollView={true}
-        onSnapToItem={(index) => setIndex(index)}
       />
     </SafeAreaView>
   );
